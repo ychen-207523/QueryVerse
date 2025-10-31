86070adc13133e25f83f4ebee621a9bf59fcfe47
Yunfei Chen <ychen207523@gmail.com>
2025-10-30 22:03:54 -0400

    initialize project

---
+++ backend/src/main/java/com/queryverse/backend/QueryVerseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.queryverse.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryVerseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryVerseBackendApplication.class, args);
+	}
+
+}
